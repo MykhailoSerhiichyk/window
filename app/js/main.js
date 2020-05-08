00761df5b924b4fee22fb54658f847d7276099e2
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-08 11:43:13 +0300

    added section range

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-    
+    $('.range-content__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+          $('.range-content__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+          $('.range-content__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+          $(this).addClass('active');
+          $('#'+id).addClass('active-tab').fadeIn();
+          return false;
+    });
 
 });
 
