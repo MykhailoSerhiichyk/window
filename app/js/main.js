3c105b579059444a7b24c3897621a62edf180dc1
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-08 13:06:10 +0300

    added section sale

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,15 @@ $(function(){
           return false;
     });
 
+    $('.sale-content__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+          $('.sale-content__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+          $('.sale-content__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+          $(this).addClass('active');
+          $('#'+id).addClass('active-tab').fadeIn();
+          return false;
+    });
+
 });
 
 
